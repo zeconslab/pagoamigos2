5ab00b097a34f2a772ba75790f23ab058907bd1b
Raul Pimentel <60498478+zeconslab@users.noreply.github.com>
2026-01-19 14:43:24 -0600

    Commit inicial

---
+++ src/main/java/com/examplo/pagoamigos/PagoamigosApplication.java
@@ -0,0 +1,13 @@
+package com.examplo.pagoamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagoamigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagoamigosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplo/pagoamigos/PagoamigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplo.pagoamigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagoamigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
