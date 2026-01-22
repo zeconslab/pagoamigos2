e312a1ca64b9fa8eb72cf8ec0443ac5058b1d7ff
Raul Pimentel <60498478+zeconslab@users.noreply.github.com>
2026-01-22 11:19:11 -0600

    ajustes dashboard

--- src/main/java/com/examplo/pagoamigos/model/MonthlyPayment.java
+++ src/main/java/com/examplo/pagoamigos/model/MonthlyPayment.java
@@ -10,12 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Transient;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
